e26ebccf566bdee428b724aa8555f75a4eb44d53
Jagnath <chatnallij@gmail.com>
2023-01-27 20:47:46 +0530

    adding the string class

---
+++ src/test/java/day3/ExampleString1.java
@@ -0,0 +1,10 @@
+package day3;
+
+public class ExampleString1 {
+    public static void main(String[] args) {
+
+        String name="Jagnath";
+        System.out.println(name.equals("jagnath"));
+        System.out.println(name.equalsIgnoreCase("jagnath"));
+    }
+}
---
+++ src/test/java/day3/ExampleString3.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class ExampleString3 {
+
+    public static void main(String[] args) {
+
+        String name = "Im a software engineering";
+
+        System.out.println(name.contains("a"));
+        System.out.println(name.startsWith("Im"));
+        System.out.println(name.endsWith("ng"));
+    }
+}
