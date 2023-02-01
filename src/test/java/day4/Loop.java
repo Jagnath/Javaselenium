c98c804b89449a516c5b637beb62f91bc9e5fb19
Jagnath <chatnallij@gmail.com>
2023-02-01 21:42:59 +0530

    adding the loop

---
+++ src/test/java/day4/Loop.java
@@ -0,0 +1,23 @@
+package day4;
+
+public class Loop {
+    public static void main(String[] args) {
+        String names = "Mallu";
+
+        String[] Names = {"Mallu", "Ganesh", "Sachin", "Omkar"};
+
+        for (String ref : Names) {
+            if (ref.equals("Mallu")){
+                System.out.println(ref);
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
