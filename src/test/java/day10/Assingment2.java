760cef2997dcd9163e480514bec340fa464f0bf8
Jagnath <chatnallij@gmail.com>
2023-02-10 19:19:57 +0530

    Assignment

---
+++ src/test/java/day10/Assingment2.java
@@ -0,0 +1,20 @@
+package day10;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Assingment2 {
+
+    public static void main(String[] args) throws IOException {
+
+        String path = System.getProperty("user.dir")+ File.separator+"JagnathNotes";
+        File fl=new File(path);
+        FileWriter write=new FileWriter(fl);
+        BufferedWriter wr= new BufferedWriter(write);
+        wr.write("Jagnath");
+        wr.flush();
+
+    }
+}
